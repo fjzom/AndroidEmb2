ed31dd9da2ea84696d160b255a2e31edf73f261a
fjzom <f.oviedo.m@hotmail.com>
2015-05-25 22:17:54 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	MyApplication2/.idea/gradle.xml
    
    	MyApplication2/app/src/main/java/com/mycompany/myapplication/MainActivity.java

--- MyApplication2/app/src/main/java/com/mycompany/myapplication/detailActivity.java
+++ MyApplication2/app/src/main/java/com/mycompany/myapplication/detailActivity.java
@@ -1,35 +1,56 @@
 package com.mycompany.myapplication;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class detailActivity extends ActionBarActivity {
+public class detailActivity extends Activity implements OnClickListener {
 
-    public TextView nomTask,mat,dateTask,timeTask,descTask;
+
+    public EditText searchTarea,searchMateria,searchDescripcion;
+    public TextView searchTime,searchDate;
+    private DbHelper dbHelper;
+    private SQLiteDatabase database;
+    private Button deleteBtn, updateBtn;
+    private long _id;
+    private String _idS;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
 
         // Get the message from the intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        nomTask = (TextView) findViewById(R.id.searchTarea);
-        mat = (TextView) findViewById(R.id.searchMateria);
-        dateTask = (TextView) findViewById(R.id.searchTime);
-        timeTask = (TextView) findViewById(R.id.searchDate);
-        descTask = (TextView) findViewById(R.id.searchDescripcion);
-        DbHelper dbh = new DbHelper(this);
-        SQLiteDatabase db = dbh.getWritableDatabase();
+
+        final DbHelper dbh = new DbHelper(this);
+        final SQLiteDatabase db = dbh.getWritableDatabase();
+        searchTarea = (EditText) findViewById(R.id.searchTarea);
+        searchMateria = (EditText) findViewById(R.id.searchMateria);
+        searchTime = (TextView) findViewById(R.id.txtTimePicker);
+        searchDate = (TextView) findViewById(R.id.txtDatePicker);
+        searchDescripcion = (EditText) findViewById(R.id.searchDescripcion);
 
         String[] campos = new String[]{"tarea_id", "nombre", "descripcion", "materia", "hora", "fecha"};
         String[] args = new String[]{message};
@@ -38,28 +59,33 @@ public class detailActivity extends ActionBarActivity {
 
 
         if (c.moveToFirst()) {
+            String tareaIdTaskSearch = c.getString(0);
             String nombreTaskSearch = c.getString(1);
             String descripcionTaskSearch = c.getString(2);
             String materiaTaskSearch = c.getString(3);
             String horaTaskSearch = c.getString(4);
             String dateTaskSearch = c.getString(5);
 
-            nomTask.setText(nombreTaskSearch);
-            mat.setText(materiaTaskSearch);
-            dateTask.setText(dateTaskSearch);
-            timeTask.setText(horaTaskSearch);
-            descTask.setText(descripcionTaskSearch);
+            searchTarea.setText(nombreTaskSearch);
+            searchMateria.setText(materiaTaskSearch);
+            searchDate.setText(dateTaskSearch);
+            searchTime.setText(horaTaskSearch);
+            searchDescripcion.setText(descripcionTaskSearch);
 
-
+            _idS = tareaIdTaskSearch;
+            _id = Long.parseLong(tareaIdTaskSearch);
         } else {
 
             Toast.makeText(getApplicationContext(),
                     "Ha ocurrido un error :(", Toast.LENGTH_LONG).show();
             db.close();
         }
+        updateBtn = (Button)findViewById(R.id.update_btn);
+        deleteBtn = (Button)findViewById(R.id.delete_btn);
+        updateBtn.setOnClickListener(this);
+        deleteBtn.setOnClickListener(this);
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,4 +108,102 @@ public class detailActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        DbHelper admin = new DbHelper(this,"agendadb", null, 1);
+        final SQLiteDatabase bd = admin.getWritableDatabase();
+
+
+        switch(v.getId()){
+            case R.id.delete_btn:
+                AlertDialog.Builder deleteMessage = new AlertDialog.Builder(this);
+                deleteMessage.setMessage(R.string.confirmDelReg);
+                deleteMessage.setPositiveButton(R.string.btn_Yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        try {
+                            bd.delete("tarea", "tarea_id" + "=" + _id, null);
+                            Toast.makeText(getApplicationContext(), "Tarea borrada",
+                                    Toast.LENGTH_LONG).show();
+                            bd.close();
+                            dialog.dismiss();
+                            returnHome();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error ",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+                deleteMessage.setNegativeButton(R.string.btn_No, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                deleteMessage.show();
+
+
+                break;
+            case R.id.update_btn:
+                AlertDialog.Builder  updateMessage = new AlertDialog.Builder(this);
+                updateMessage.setMessage(R.string.confirmUpdateReg);
+                updateMessage.setPositiveButton(R.string.btn_Yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        try {
+                            ContentValues cvUpdates = new ContentValues();
+                            String upsearchTarea = searchTarea.getText().toString();
+                            String upsearchMateria = searchMateria.getText().toString();
+                            String upsearchTime = searchTime.getText().toString();
+                            String upsearchDate = searchDate.getText().toString();
+                            String upsearchDescripcion = searchDescripcion.getText().toString();
+
+                            cvUpdates.put("nombre",upsearchTarea);
+                            cvUpdates.put("materia",upsearchMateria);
+                            cvUpdates.put("hora",upsearchTime);
+                            cvUpdates.put("fecha",upsearchDate);
+                            cvUpdates.put("descripcion",upsearchDescripcion);
+                            bd.update("tarea", cvUpdates, "tarea_id" + "=" + _id, null);
+                            Toast.makeText(getApplicationContext(), "Tarea actualziada",
+                                    Toast.LENGTH_LONG).show();
+                            bd.close();
+                            dialog.dismiss();
+                            returnHome();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error ",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+                updateMessage.setNegativeButton(R.string.btn_No, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                updateMessage.show();
+
+
+
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    public void returnHome() {
+        Intent home_intent = new Intent(getApplicationContext(),
+               MainActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(home_intent);
+    }
+
 }
